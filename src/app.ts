7d40c8ef667773ccd1e6978ec4078454fa2454f8
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-11-18 19:18:36 +0600

    prjoect create and some route and data in mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoute } from './app/config/modules/student/student.route';
 const app: Application = express();
 
 //! parsers
 app.use(express.json());
 app.use(cors());
 
+// application routes
+app.use('/api/v1/students', StudentRoute);
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
-
 export default app;
